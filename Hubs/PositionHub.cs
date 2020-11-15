397c9d0be3965feb9d8a6268915c858d58ab3732
Marco Mendez <mmendez@carmentaglobal.com>
2020-11-15 10:49:17 -0600

    Add project files.

---
+++ Hubs/PositionHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignnalRFirst.Hubs
+{
+    public class PositionHub : Hub
+    {
+        public async Task SendPosition(int left, int top)
+        {
+            //Hubs o concentradores son la clave
+            // Clients da la funcionalidad a quien se desea enviar
+            await Clients.Others.SendAsync("ReceivePosition",left,top);
+        }
+    }
+}
